4d01b02d106d54d1a721768344f178779432174c
tsiupaknazar <51015185+tsiupaknazar@users.noreply.github.com>
2021-10-10 20:56:14 +0300

    Created seats scheme, added some interactive

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const schemeSvg = document.querySelector(".scheme-svg");
+const priceTag = document.querySelector(".price-total");
+
+let cost = 500;
+let totalPrice = 0;
+
+schemeSvg.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("booked")) {
+    event.target.classList.toggle("active");
+    let totalSeats = schemeSvg.querySelectorAll(".active").length;
+    totalPrice = totalSeats * cost;
+    priceTag.textContent = totalPrice;
+  }
+});
